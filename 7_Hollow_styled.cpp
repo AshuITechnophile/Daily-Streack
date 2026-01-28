daf8d3865f96cbefcf653083f24e7e2ce677bdcb
Ashu <ashutosh.mihra.royal.max@gmail.com>
2026-01-28 08:35:15 +0530

    Hollow styled star pattern

---
+++ 7_Hollow_styled.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1 || i == 5)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                cout << "* ";
+            }
+        }
+        else
+        {
+            for (int k = 1; k <= n; k++)
+            {
+                if (k == 1 || k == 5)
+                {
+                    cout << "* ";
+                }
+                else
+                {
+                    cout << "  ";
+                }
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
